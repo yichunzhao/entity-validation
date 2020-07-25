20c1d5da1fa92bfb56a52f2c8fe50b6fbf6a0fb2
yichunzhao <zhaoyichun@hotmail.com>
2020-07-25 02:38:27 +0200

    return error message, including the validated field.

--- src/main/java/com/ynz/entityautovalidation/GlobalExceptionHandler.java
+++ src/main/java/com/ynz/entityautovalidation/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ynz.entityautovalidation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionSystemException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.persistence.RollbackException;
 import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestControllerAdvice
@@ -47,12 +50,14 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler
-    public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        List<String> errors = new ArrayList<>();
-        String message = METHOD_ARG_INVALID_MSG;
-        e.getBindingResult().getAllErrors().forEach(err -> errors.add(err.getDefaultMessage()));
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
 
-        return new ResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, message, errors), HttpStatus.BAD_REQUEST);
+        String message = METHOD_ARG_INVALID_MSG;
+        e.getBindingResult().getAllErrors().forEach(err ->
+                errors.put(((FieldError) err).getField(), err.getDefaultMessage()));
+
+        return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
